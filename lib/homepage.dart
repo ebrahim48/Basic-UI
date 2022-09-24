ed1e033d4f4b6c6bda180e9535b0dc666d9c0f4a
ebrahim <ebrahim@softbyteit.com>
2022-09-24 17:34:09 +0600

    stack and positioned add

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,7 +5,7 @@ import 'custom_container.dart';
 import 'main.dart';
 
 
-const String placeHolder='https://www.pngkey.com/png/full/349-3499617_person-placeholder-person-placeholder.png';
+const String imageUrl='https://i.pinimg.com/originals/0b/f9/8e/0bf98ec36bd8421f4580e5708a0c9ed7.jpg';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -20,93 +20,35 @@ class HomePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Stack(
+                _buildImageSection(),
+                _titleSection(),
+                _navigationSection(),
+                Row(
                   children: [
-                    Image.network(
-                      imageUrl,
-                      width: double.infinity,
-                      height: 300,
-                      fit: BoxFit.cover,
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.purple,
+                      ),
                     ),
-                    Card(
-                      elevation: 10,
-                      child: Image.network(
-                          placeHolder,
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
+                    Expanded(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.green,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.red,
                       ),
                     )
+                  ],
 
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    onTap: () {
-                      print('Item Clicked');
-                    },
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                    tileColor: Colors.grey,
-                    title: Text('My Beautiful Car'),
-                    subtitle: Text('Mohammadpur,Dhaka, Bangladesh'),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(Icons.star,color: Colors.red,),
-                        Text('99')
-                      ],
-                    ),
-                    leading: Icon(Icons.favorite,color: Colors.redAccent,),
-                  ),
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    MyContainer(
-                      onPressed: () {
-                        print('Calling...');
-                      },
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(Icons.call,
-                            color: Colors.black,),
-                          SizedBox(height: 5,),
-                          Text('CALL'),
-                        ],
-                      ),
-                    ),
-                    MyContainer(
-                      onPressed: () {
-                        print('Opening Map...');
-                      },
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(Icons.navigation,
-                            color: Colors.black,),
-                          SizedBox(height: 5,),
-                          Text('ROUTE'),
-                        ],
-                      ),
-                    ),
-                    MyContainer(
-                      onPressed: () {
-                        print('Sharing...');
-                      },
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(Icons.share,
-                            color: Colors.black,),
-                          SizedBox(height: 5,),
-                          Text('SHARE'),
-                        ],
-                      ),
-                    ),
-                  ],
                 ),
                 Padding(
                   padding: const EdgeInsets.all(16.0),
@@ -118,5 +60,156 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Row _navigationSection() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        MyContainer(
+          onPressed: () {
+            print('Calling...');
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.call,
+                color: Colors.black,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text('CALL'),
+            ],
+          ),
+        ),
+        MyContainer(
+          onPressed: () {
+            print('Opening Map...');
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.navigation,
+                color: Colors.black,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text('ROUTE'),
+            ],
+          ),
+        ),
+        MyContainer(
+          onPressed: () {
+            print('Sharing...');
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.share,
+                color: Colors.black,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text('SHARE'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Padding _titleSection() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListTile(
+        onTap: () {
+          print('Item Clicked');
+        },
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        tileColor: Colors.grey,
+        title: Text('My Beautiful Car'),
+        subtitle: Text('Mohammadpur,Dhaka, Bangladesh'),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.star,
+              color: Colors.purple,
+            ),
+            Text('99')
+          ],
+        ),
+        leading: Icon(
+          Icons.favorite,
+          color: Colors.redAccent,
+        ),
+      ),
+    );
+  }
+
+  Stack _buildImageSection() {
+    return Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Image.network(
+                    imageUrl,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                  Positioned(
+                    top: 0,
+                    bottom: 0,
+                      right: 0,
+                      left: 0,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)
+                            ),
+                            elevation: 10,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(50),
+                              child: Image.network(
+                                imageUrl,
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          Text('My Beautiful Car',
+                            style: TextStyle(
+                              backgroundColor: Colors.black54,
+                              fontSize: 25,
+                              color: Colors.white,
+                            ),)
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  Positioned(
+                      bottom: -40,
+                      right: 10,
+                      child: Icon(
+                        Icons.favorite,
+                        size: 80,
+                        color: Colors.red,))
+
+
+                ],
+              );
   }
 }
